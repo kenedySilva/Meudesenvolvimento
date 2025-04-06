16e2f75538791ac9db7f8e78219fe55329ca7a79
kenedyaps <kenedysilvagit@gmail.com>
2025-04-06 11:47:27 -0300

    Arquivos JS de Desafios
    Desafios Feitos no curso referente a Desenvolvimento Web js

---
+++ desafio_area.js
@@ -0,0 +1,7 @@
+{
+    var raio  = 10;
+    var valorArea = Math.PI * raio * raio;
+}
+{
+    console.log("A area da circuferencia é "+ valorArea +"M²");
+}
---
+++ desafio_troca.js
@@ -0,0 +1,10 @@
+var a = 7;
+var b = 94;
+
+console.log(a);
+console.log(b);
+var x = a;
+var a = b;
+var b = x;
+console.log(a);
+console.log(b);
